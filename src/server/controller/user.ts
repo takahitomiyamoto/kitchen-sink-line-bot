113268a4daa150bbc97daf0503374c7e419f03de
takahito.miyamoto <takahito.miyamoto@accenture.com>
2018-05-07 22:39:11 +0900

    change post

--- src/server/controller/user.ts
+++ src/server/controller/user.ts
@@ -1,8 +1,11 @@
-import { interfaces, controller, httpGet } from 'inversify-express-utils';
+import { interfaces, controller, httpGet, httpPost } from 'inversify-express-utils';
 import { inject } from '../ioc/ioc';
 import { UserService } from '../service/user';
 import { User } from '../models/user';
 import TYPES from '../constant/types';
+import * as line from '@line/bot-sdk';
+import { configLine } from '../config/line';
+import { LineUtils as lineUtils } from '../utils/line';
 
 @controller('/user')
 export class UserController implements interfaces.Controller  {
@@ -13,4 +16,26 @@ export class UserController implements interfaces.Controller  {
   public getUsers(): Promise<User[]> {
     return this.userService.getUsers();
   }
+
+  @httpPost('/callback')
+  public callback(req, res) {
+    console.log(line.middleware(configLine));
+    console.log('configLine: ' + configLine);
+    console.log('req: ' + req);
+    console.log('res: ' + res);
+    // req.body.events should be an array of events
+    if (!Array.isArray(req.body.events)) {
+      return res.status(500).end();
+    }
+    // handle events separately
+    // Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
+    Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
+      .then(() => res.end())
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      })
+    ;
+  }
+
 }
--- src/server/webapi.ts
+++ src/server/webapi.ts
@@ -14,32 +14,32 @@ export class WebApi {
    */
   constructor() {
     this._express = Express()
-    //   .use(BodyParser.json())
-    //   .use(BodyParser.urlencoded({ extended: true }))
-    //   .use(helmet())
+      .use(BodyParser.urlencoded({ extended: true }))
+      .use(BodyParser.json())
+      .use(helmet())
     //   .use(cors())
       // serve static and downloaded files
       .use('/static', Express.static('static'))
       .use('/downloaded', Express.static('downloaded'))
       // webhook callback
-      .post('/callback', line.middleware(configLine), (req: Express.Request, res: Express.Response) => {
-        console.log('configLine: ' + configLine);
-        console.log('req: ' + req);
-        console.log('res: ' + res);
-              // req.body.events should be an array of events
-        if (!Array.isArray(req.body.events)) {
-          return res.status(500).end();
-        }
-        // handle events separately
-        // Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
-        Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
-          .then(() => res.end())
-          .catch((err) => {
-            console.error(err);
-            res.status(500).end();
-          })
-        ;
-      })
+    //   .post('/callback', line.middleware(configLine), (req: Express.Request, res: Express.Response) => {
+    //     console.log('configLine: ' + configLine);
+    //     console.log('req: ' + req);
+    //     console.log('res: ' + res);
+    //           // req.body.events should be an array of events
+    //     if (!Array.isArray(req.body.events)) {
+    //       return res.status(500).end();
+    //     }
+    //     // handle events separately
+    //     // Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
+    //     Promise.all(req.body.events.map(lineUtils.instance.handleEvent))
+    //       .then(() => res.end())
+    //       .catch((err) => {
+    //         console.error(err);
+    //         res.status(500).end();
+    //       })
+    //     ;
+    //   })
     ;
   }
 
