ed816a1763434869dcb9e0b6d99ffcc94a018502
takahito.miyamoto <takahito.miyamoto@accenture.com>
2018-05-10 21:14:17 +0900

    fix

--- src/server/service/vision.ts
+++ src/server/service/vision.ts
@@ -4,12 +4,34 @@ import * as jwt from 'jsonwebtoken';
 
 export class VisionService {
   private static _visionService: VisionService = new VisionService();
+  private static _accessToken = VisionService.getAccessToken();
+
   private constructor() {}
   public static get instance(): VisionService {
     return VisionService._visionService;
   }
 
-  private createTokenOptions() {
+  public static get accessToken() {
+    return VisionService._accessToken;
+  }
+
+  private static getAccessToken() {
+    const tokenOptions = VisionService.createTokenOptions();
+    return new Promise((resolve, reject) => {
+      rp(tokenOptions)
+        .then((data) => {
+          const accessToken = data['access_token'];
+          resolve(accessToken)
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        })
+      ;
+    });
+  }
+
+  private static createTokenOptions() {
     const url = process.env.EINSTEIN_VISION_URL + process.env.EINSTEIN_API_VERSION;
     const private_key = process.env.EINSTEIN_VISION_PRIVATE_KEY;
     const account_id = process.env.EINSTEIN_VISION_ACCOUNT_ID;
@@ -66,51 +88,49 @@ export class VisionService {
     return options;
   }
 
-  private getAccessToken(options) {
-    return new Promise((resolve, reject) => {
-      // let accessToken;
-      rp(options)
-        .then((data) => {
-          // const data = JSON.parse(body);
-          const accessToken = data['access_token'];
-          // console.log('0: accessToken: ' + accessToken);
-          resolve(accessToken)
-        })
-        .catch((err) => {
-          console.log(err);
-          reject(err);
-        })
-      ;
-    });
-  }
+  // private static getAccessToken(options) {
+  //   return new Promise((resolve, reject) => {
+  //     // let accessToken;
+  //     rp(options)
+  //       .then((data) => {
+  //         // const data = JSON.parse(body);
+  //         const accessToken = data['access_token'];
+  //         // console.log('0: accessToken: ' + accessToken);
+  //         resolve(accessToken)
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         reject(err);
+  //       })
+  //     ;
+  //   });
+  // }
+
+  // private static createTokenOptions = new Promise((resolve, reject) => {
+  //   console.log('#################### 1. createTokenOptions ####################');
+  //   const tokenOptions = this.createTokenOptions();
+  //   resolve(tokenOptions);
+  // });
+
+  // private static getAccessToken = (tokenOptions, counter) => new Promise((resolve, reject) => {
+  //   console.log('#################### 2. getAccessToken ####################');
+  //   console.log('#################### counter: ' + counter);
+  //   // 2回目以降は取得しない
+  //   if (counter > 1) {
+  //     return;
+  //   }
+  //   const ACCESS_TOKEN = this.getAccessToken(tokenOptions);
+  //   resolve(ACCESS_TOKEN);
+  // });
 
   public imageClassify = (targetImage) => {
-    const createTokenOptions = new Promise((resolve, reject) => {
-      console.log('#################### 1. createTokenOptions ####################');
-      const tokenOptions = this.createTokenOptions();
-      resolve(tokenOptions);
-    });
-
-    const getAccessToken = (tokenOptions, counter) => new Promise((resolve, reject) => {
-      console.log('#################### 2. getAccessToken ####################');
-      console.log('#################### counter: ' + counter);
-      // 2回目以降は取得しない
-      if (counter > 1) {
-        return;
-      }
-      const ACCESS_TOKEN = this.getAccessToken(tokenOptions);
-      resolve(ACCESS_TOKEN);
-    });
-
-    const createPredictOptions = (accessToken) => new Promise((resolve, reject) => {
-      console.log('#################### 3. createPredictOptions ####################');
-      const predictOpitions = this.createPredictOptions(targetImage, accessToken);
-      console.log('#################### accessToken: ' + accessToken);
-      resolve(predictOpitions);
-    });
-
+    // const createPredictOptions = (accessToken) => new Promise((resolve, reject) => {
+      //   console.log('#################### 3. createPredictOptions ####################');
+      //   const predictOpitions = this.createPredictOptions(targetImage, accessToken);
+      //   console.log('#################### accessToken: ' + accessToken);
+      //   resolve(predictOpitions);
+      // });
     const requestPredict = (predictOpitions) => new Promise((resolve, reject) => {
-      console.log('#################### 4. requestPredict ####################');
       rp(predictOpitions)
         .then((body) => {
           // POST succeeded...
@@ -124,17 +144,21 @@ export class VisionService {
       ;
     });
 
-    let counter = 0;
     return new Promise((resolve, reject) => {
-      createTokenOptions.then((tokenOptions) => {
-        counter++;
-        return getAccessToken(tokenOptions, counter);
-      }).then((accessToken) => {
-        return createPredictOptions(accessToken);
-      }).then((predictOpitions) => {
-        return requestPredict(predictOpitions);
-      });
+      const accessToken = VisionService.accessToken;
+      console.log('accessToken: ' + accessToken);
+      const predictOpitions = this.createPredictOptions(targetImage, accessToken);
+      requestPredict(predictOpitions);
     });
+
+    // return new Promise((resolve, reject) => {
+    //   const accessToken = VisionService.accessToken;
+    //   console.log('accessToken: ' + accessToken);
+    //   return createPredictOptions(accessToken);
+    // }).then((predictOpitions) => {
+    //   return requestPredict(predictOpitions);
+    //   });
+    // });
     // let counter = 0;
     // return new Promise((resolve, reject) => {
     //   this.logger('createTokenOptions: ' + counter);
