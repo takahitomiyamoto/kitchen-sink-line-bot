6a8ab9088c925603f592d1adea6b199bea3825e7
takahito.miyamoto <takahito.miyamoto@accenture.com>
2018-05-13 09:53:04 +0900

    fix

--- src/server/service/line.ts
+++ src/server/service/line.ts
@@ -164,31 +164,31 @@ export class LineService {
     return this.client.replyMessage(
       replyToken,
       // [
-      // {
-      //   type: 'sticker',
-      //   packageId: '4', //message.packageId,
-      //   stickerId: '293' //message.stickerId,
-      // },
       {
-        type: 'template',
-        altText: 'defaultMessage',
-        template: {
-          type: 'confirm',
-          text: message.SORRY_TELL_ME_AGAIN_JA,
-          actions: [
-            {
-              label: message.BUTTONS_YES_JA,
-              type: 'message',
-              text: message.BUTTONS_YES_JA
-            },
-            {
-              label: message.BUTTONS_NO_JA,
-              type: 'message',
-              text: message.BUTTONS_NO_JA
-            }
-          ]
-        }
-      }
+        type: 'sticker',
+        packageId: '4', //message.packageId,
+        stickerId: '293' //message.stickerId,
+      },
+      // {
+      //   type: 'template',
+      //   altText: 'defaultMessage',
+      //   template: {
+      //     type: 'confirm',
+      //     text: message.SORRY_TELL_ME_AGAIN_JA,
+      //     actions: [
+      //       {
+      //         label: message.BUTTONS_YES_JA,
+      //         type: 'message',
+      //         text: message.BUTTONS_YES_JA
+      //       },
+      //       {
+      //         label: message.BUTTONS_NO_JA,
+      //         type: 'message',
+      //         text: message.BUTTONS_NO_JA
+      //       }
+      //     ]
+      //   }
+      // }
     // ]
     );
   }
