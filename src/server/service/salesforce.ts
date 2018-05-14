62d3fae1b3e846c3d45782ba05c75a05dfeb6122
takahito.miyamoto <takahito.miyamoto@accenture.com>
2018-05-14 22:56:59 +0900

    fix

--- src/server/service/salesforce.ts
+++ src/server/service/salesforce.ts
@@ -53,7 +53,7 @@ export class SalesforceService {
       });
     }).then(() => {
       console.log('Successfully inserted.');
-    }).err((err) =>{
+    }).catch((err) =>{
       console.error(err);
     });
   }
